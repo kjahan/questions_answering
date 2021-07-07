1d00d286eddb4f8cd3a983e56662b22c1854a689
Kazem Jahanbakhsh <k.jahanbakhsh@gmail.com>
2021-07-07 16:54:45 -0700

    test medical use cases

--- run_experiments.py
+++ run_experiments.py
@@ -6,6 +6,10 @@ import experiments.mathematics as mathematics
 import experiments.chemistry as chemistry
 import experiments.guardian_of_galaxy as guardian
 import experiments.manhattan as manhattan
+import experiments.chemo as chemo
+import experiments.asthma as asthma
+import experiments.covid_19 as covid_19
+import experiments.covid_vaccine as covid_vaccine
 
 
 # eliza.run_experiment()
@@ -13,7 +17,12 @@ import experiments.manhattan as manhattan
 # trump.run_experiment()
 # ss.run_experiment()
 # guardian.run_experiment()
-manhattan.run_experiment()
+# manhattan.run_experiment()
 # mathematics.run_triangle_experiment()
 # chemistry.run_oxygen_experiment()
 # chemistry.run_morphine_experiment()
+# chemo.run_experiment()
+# acetaminophen.run_experiment()
+# asthma.run_experiment()
+# covid_19.run_experiment()
+covid_vaccine.run_experiment()
